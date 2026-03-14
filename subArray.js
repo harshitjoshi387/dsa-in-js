a19652fe8aeb81a7f6b8c1bca3433015c9f1403c
Harshit joshi <harshjoshi25022002@gmail.com>
2026-03-14 09:09:50 +0530

    sumofsubArray

--- subArray.js
+++ subArray.js
@@ -1,9 +1,11 @@
 let arr=[1,2,3,7,5]
 let target=12;
-let count = 0
+let count=0
 for(let i=0;i<arr.length;i++){
+        let sum=0
     for(let j=i+1;j<arr.length;j++){
-        if(arr[i]+arr[j]==target){
+        sum+= arr[j]
+        if(sum==target){
             count++
         }
     }

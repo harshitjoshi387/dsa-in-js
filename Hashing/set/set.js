b757a1c5c98affd131cb14d00405c72eeb696a9f
Harshit joshi <harshjoshi25022002@gmail.com>
2026-07-18 20:03:37 +0530

    implementation of set

--- Hashing/set/set.js
+++ Hashing/set/set.js
@@ -1,10 +1,21 @@
+// let set = new Set()
+
+// set.add(10)
+// set.add(20)
+// set.add(30)
+// set.add(40)
+// set.add(10)
+
+
+// console.log(set)
+
+
+
+let arr=[1,2,3,1,5,2,3,8]
 let set = new Set()
 
-set.add(10)
-set.add(20)
-set.add(30)
-set.add(40)
-set.add(10)
 
-
-console.log(set)
+for(let i=0;i<arr.length;i++){
+    set.add(arr[i])
+}
+console.log(set);

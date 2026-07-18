3d2785f5c025133b4078c78c77bc7ff3affc7091
Harshit joshi <harshjoshi25022002@gmail.com>
2026-07-18 20:10:14 +0530

    remove duplicates

--- Hashing/set/set.js
+++ Hashing/set/set.js
@@ -11,11 +11,28 @@
 
 
 
-let arr=[1,2,3,1,5,2,3,8]
+// let arr=[1,2,3,1,5,2,3,8]
+// let set = new Set()
+
+
+// for(let i=0;i<arr.length;i++){
+//     set.add(arr[i])
+// }
+// console.log(set);
+
+
+
+let arr= [1,3,4,5,3,3,4]
+
 let set = new Set()
 
 
-for(let i=0;i<arr.length;i++){
-    set.add(arr[i])
+for (let i=0;i<arr.length;i++){
+    if(set.has(arr[i])){
+        set.delete(arr[i])
+    }
+    else set.add(arr[i])
 }
+
 console.log(set);
+

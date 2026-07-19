a5de41c381a567ab1425f5da988a435c8eea2514
Harshit joshi <harshjoshi25022002@gmail.com>
2026-07-19 11:29:50 +0530

    frequency of char using the Map data Structure

--- Hashing/Map/map.js
+++ Hashing/Map/map.js
@@ -1,12 +1,32 @@
-let map = new Map();
+// let map = new Map();
 
-map.set("ajay", 5);
-map.set("amit", 7);
-map.set("aman", 3);
-map.set("skashi", 8);
-map.set("ajay", 2);   
+// map.set("ajay", 5);
+// map.set("amit", 7);
+// map.set("aman", 3);
+// map.set("skashi", 8);
+// map.set("ajay", 2);   
 
-console.log(map);
-map.delete("aman")
+// console.log(map);
+// map.delete("aman")
 
-console.log(map)
+// console.log(map.size)
+
+// console.log(map)
+
+
+// for(let value of map.values()){
+//     console.log(value)
+// }
+
+let arr = [1,2,3,4,5,2,1,3,4,1]
+let freqMap = new Map();
+
+for (let char of arr) {
+  if (freqMap.has(char)) {
+    freqMap.set(char, freqMap.get(char) + 1);  
+  } else {
+    freqMap.set(char, 1);  
+  }
+}
+
+console.log(freqMap);
